e70d3d3ad0a6751a9036c90c6b9b667a66b264c3
DESKTOP-JJ5ARMP\ADMIN <quynhnhu18121812@gmail.com>
2023-04-07 10:11:12 +0700

    commit message

---
+++ javascript/login_screen_VS02.js
@@ -0,0 +1,14 @@
+var x = document.getElementById("login_from");
+var y = document.getElementById("register_form");
+var z = document.getElementById("btn");
+
+function register_click(){
+    x.style.left = "-700px";
+    y.style.right = "15%";
+    z.style.left = "50%";
+};
+function login_click(){
+    x.style.left = "15%";
+    y.style.right = "700px";
+    z.style.left = "0";
+};
