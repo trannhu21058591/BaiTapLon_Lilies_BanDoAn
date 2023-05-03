d57c7bf2b001af591cd5927b2e3cfc53e8cfd15e
DESKTOP-JJ5ARMP\ADMIN <quynhnhu18121812@gmail.com>
2023-05-03 21:35:35 +0700

    commit message

---
+++ javascript/admin.js
@@ -0,0 +1,109 @@
+var barChartOptions = {
+    series: [{
+      data: [10, 8, 6, 4, 2]
+    }],
+    chart: {
+      type: 'bar',
+      height: 350,
+      toolbar: {
+        show: false
+      },
+    },
+    colors: [
+      "#246dec",
+      "#cc3c43",
+      "#367952",
+      "#f5b74f",
+      "#4f35a1"
+    ],
+    plotOptions: {
+      bar: {
+        distributed: true,
+        borderRadius: 4,
+        horizontal: false,
+        columnWidth: '40%',
+      }
+    },
+    dataLabels: {
+      enabled: false
+    },
+    legend: {
+      show: false
+    },
+    xaxis: {
+      categories: ["BBQ Burger", "Curly Fries", "Spaghetti", "Lemonade", "Linguine"],
+    },
+    yaxis: {
+      title: {
+        text: "Count"
+      }
+    }
+  };
+  
+  var barChart = new ApexCharts(document.querySelector("#bar-chart"), barChartOptions);
+  barChart.render();
+  
+  
+  // AREA CHART
+  var areaChartOptions = {
+    series: [{
+      name: 'Purchase Orders',
+      data: [31, 40, 28, 51, 42, 109, 100]
+    }, {
+      name: 'Sales Orders',
+      data: [11, 32, 45, 32, 34, 52, 41]
+    }],
+    chart: {
+      height: 350,
+      type: 'area',
+      toolbar: {
+        show: false,
+      },
+    },
+    colors: ["#4f35a1", "#246dec"],
+    dataLabels: {
+      enabled: false,
+    },
+    stroke: {
+      curve: 'smooth'
+    },
+    labels: ["Jan", "Feb", "Mar", "Apr", "May", "Jun", "Jul"],
+    markers: {
+      size: 0
+    },
+    yaxis: [
+      {
+        title: {
+          text: 'Purchase Orders',
+        },
+      },
+      {
+        opposite: true,
+        title: {
+          text: 'Sales Orders',
+        },
+      },
+    ],
+    tooltip: {
+      shared: true,
+      intersect: false,
+    }
+  };
+  
+  var areaChart = new ApexCharts(document.querySelector("#area-chart"), areaChartOptions);
+  areaChart.render();
+
+
+
+const menu_image = document.querySelector(".menu_small");
+const drop_menu_small = document.querySelector(".menu_small_list");
+
+menu_image.onclick = function() {
+    drop_menu_small.classList.toggle('open_menu_small_list');
+}
+
+const btn = document.getElementById("logo_n");
+btn.addEventListener("click", function() {
+    // Chuyển đến trang mới
+    window.location.href = "/html/Welcome_screen_VS03.html";
+});
