3496121bbfd51adbd2b765d967600896d5905178
TranThiQuynhNhu_ <quynhnhu18121812@gmail.com>
2023-05-08 20:12:07 +0700

    commit message

--- javascript/profile_screen.js
+++ javascript/profile_screen.js
@@ -4,3 +4,10 @@ const drop_menu_small = document.querySelector(".menu_small_");
 menu_image.onclick = function() {
     drop_menu_small.classList.toggle('open_menu_small_');
 }
+
+let profile_IMG = document.getElementById("profile_img");
+let input_File = document.getElementById("input_file");
+
+input_File.onchange = function(){
+    profile_IMG.src = URL.createObjectURL(input_File.files[0])
+}
